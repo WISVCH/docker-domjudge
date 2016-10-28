787a26b7486d8cfeba4c1fac42dddea10a06d38f
Wim de With <nauxuron@wimdewith.com>
2016-10-28 22:06:38 +0200

    Remove checking for domserver in judgehost
    The daemon will just exit if the domserver is inaccessible. This is not
    a problem.

--- judgehost/start.sh
+++ judgehost/start.sh
@@ -2,13 +2,4 @@
 set -e
 
 /opt/domjudge/judgehost/bin/create_cgroups
-
-while ! 6<>/dev/tcp/domserver/80; do
-echo "Waiting for DOMserver to come online"
-	sleep 5
-done
-
-exec 6>&-
-exec 6<&-
-
 exec sudo -u domjudge "$@"
