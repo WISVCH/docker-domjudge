5b490201c4d2826e3be379f431b82e7d7ec03e60
Wim de With <nauxuron@wimdewith.com>
2017-03-23 17:41:17 +0100

    Do db root password check only when necessary

--- domserver/start.sh
+++ domserver/start.sh
@@ -12,10 +12,6 @@ if [ -z "$DOMJUDGE_DB_PASSWORD" ]; then
 	exit 1
 fi
 
-if [ -z "$DOMJUDGE_DB_ROOT_PASSWORD" ]; then
-	echo >&2 "Error: missing DOMJUDGE_DB_ROOT_PASSWORD environment variable"
-	exit 1
-fi
 
 printf "dummy:%s:%s:%s:%s\n" $DOMJUDGE_DB_HOST $DOMJUDGE_DB_NAME $DOMJUDGE_DB_USER $DOMJUDGE_DB_PASSWORD > /opt/domjudge/domserver/etc/dbpasswords.secret
 
@@ -28,7 +24,12 @@ exec 6>&-
 exec 6<&-
 
 sed -i 's/localhost/%/g' /opt/domjudge/domserver/bin/dj-setup-database
-if ! /opt/domjudge/domserver/bin/dj-setup-database -u root -p"$DOMJUDGE_DB_ROOT_PASSWORD" status; then
+if ! /opt/domjudge/domserver/bin/dj-setup-database -u $DOMJUDGE_DB_USER -p"$DOMJUDGE_DB_PASSWORD" status; then
+	if [ -z "$DOMJUDGE_DB_ROOT_PASSWORD" ]; then
+		echo >&2 "Error: missing DOMJUDGE_DB_ROOT_PASSWORD environment variable"
+		echo >&2 "This is only needed to setup the database once"
+		exit 1
+	fi
 	/opt/domjudge/domserver/bin/dj-setup-database -u root -p"$DOMJUDGE_DB_ROOT_PASSWORD" install
 fi
 
