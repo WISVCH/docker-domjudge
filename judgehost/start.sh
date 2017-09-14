9dc6f72f0684b8a5a18441a0b205ad290066ece3
Wim de With <wf@dewith.io>
2017-09-14 13:35:06 +0200

    [CHipCie] Set judgehost hostname from domserver

--- judgehost/start.sh
+++ judgehost/start.sh
@@ -6,6 +6,7 @@ export PATH="$PATH:$(paste -s -d ":" /opt/path)"
 : "${DOMSERVER_HOST:=http://domserver}"
 DOMSERVER_URL="${DOMSERVER_HOST}/api/v4"
 : "${DOMSERVER_USER:=judgehost}"
+DOMSERVER_HOSTNAME_API="hostname?judgehost=true"
 
 if [ -z "$DOMSERVER_PASSWORD" ]; then
 	echo >&2 "Error: missing DOMSERVER_PASSWORD environment variable"
@@ -13,6 +14,15 @@ if [ -z "$DOMSERVER_PASSWORD" ]; then
 fi
 
 printf "default\t%s\t%s\t%s\n" $DOMSERVER_URL $DOMSERVER_USER $DOMSERVER_PASSWORD > /opt/domjudge/judgehost/etc/restapi.secret
+
+id=$(curl -s -L "$DOMSERVER_URL/$DOMSERVER_HOSTNAME_API")
+if echo $id | egrep -q '^[0-9]+$'; then
+	echo "Setting hostname to judgehost$id"
+	hostname "judgehost$id"
+	grep -q "127.0.1.1\sjudgehost$id" /etc/hosts || echo "Setting entry in /etc/hosts" && echo -e "127.0.1.1\tjudgehost$id" >> /etc/hosts
+else
+	echo >&2 "API response was not a number"
+fi
 
 /opt/domjudge/judgehost/bin/create_cgroups
 exec sudo -u domjudge "$@"
