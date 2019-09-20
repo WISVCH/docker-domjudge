e9c0ae72538de348677b0f0778f4d4a6bd23ce58
Wim de With <wf@dewith.io>
2019-09-20 13:42:22 +0200

    Update CDS

--- contestdataserver/start.sh
+++ contestdataserver/start.sh
@@ -2,6 +2,7 @@
 set -e
 
 : "${ADMIN_CDS_PW:=adm1n}"
+: "${PRESADMIN_CDS_PW:=presadm1n}"
 : "${BALLOON_CDS_PW:=balloonPr1nter}"
 : "${PUBLIC_CDS_PW:=publ1c}"
 : "${PRESENTATION_CDS_PW:=presentat1on}"
@@ -9,7 +10,8 @@ set -e
 : "${LIVE_CDS_PW:=l1ve}"
 
 sed -i "s/password=\"admin_pw\"/password=\"${ADMIN_CDS_PW}\"/" /opt/wlp/usr/servers/cds/users.xml
-sed -i "s/password=\"baloon_pw\"/password=\"${BALLOON_CDS_PW}\"/" /opt/wlp/usr/servers/cds/users.xml
+sed -i "s/password=\"presadmin_pw\"/password=\"${PRESADMIN_CDS_PW}\"/" /opt/wlp/usr/servers/cds/users.xml
+sed -i "s/password=\"balloon_pw\"/password=\"${BALLOON_CDS_PW}\"/" /opt/wlp/usr/servers/cds/users.xml
 sed -i "s/password=\"public_pw\"/password=\"${PUBLIC_CDS_PW}\"/" /opt/wlp/usr/servers/cds/users.xml
 sed -i "s/password=\"icpc_pw\"/password=\"${MYICPC_CDS_PW}\"/" /opt/wlp/usr/servers/cds/users.xml
 sed -i "s/password=\"live_pw\"/password=\"${LIVE_CDS_PW}\"/" /opt/wlp/usr/servers/cds/users.xml
@@ -25,8 +27,8 @@ if [ -z "$DOMSERVER_PASSWORD" ]; then
 fi
 
 echo "using ${DOMSERVER_URL}"
-sed -i "s/user=\"domjudge_user\"/user=\"${DOMSERVER_USER}\"/" /opt/wlp/usr/servers/cds/cdsConfig.xml
-sed -i "s/password=\"domjudge_password\"/password=\"${DOMSERVER_PASSWORD}\"/" /opt/wlp/usr/servers/cds/cdsConfig.xml
-sed -i "s,url=\"contest_url\",url=\"${DOMSERVER_URL}\"," /opt/wlp/usr/servers/cds/cdsConfig.xml
+sed -i "s/user=\"domjudge_user\"/user=\"${DOMSERVER_USER}\"/" /opt/wlp/usr/servers/cds/config/cdsConfig.xml
+sed -i "s/password=\"domjudge_password\"/password=\"${DOMSERVER_PASSWORD}\"/" /opt/wlp/usr/servers/cds/config/cdsConfig.xml
+sed -i "s,url=\"contest_url\",url=\"${DOMSERVER_URL}\"," /opt/wlp/usr/servers/cds/config/cdsConfig.xml
 
 exec "$@"
